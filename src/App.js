20f8545a672ee4ca738fe7d0d373b2c0b3d3f2cb
skobraf <skobraf@gmail.com>
2019-03-01 19:43:43 +0000

    initiate file and made first api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GitRepo from './components/GitRepo';
 import './App.css';
 
+
 class App extends Component {
+  state = {
+    error:null,
+    isLoaded: false,
+    items:[]
+  }
+  apiCall = () => {
+       fetch('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.items
+            });
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        );
+        console.log(result.items)
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>
+        <GitRepo />
+        <button onClick={this.apiCall}>add</button>
+     </div>
     );
   }
 }
---
+++ src/components/GitRepo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GitRepo extends Component {
+  render() {
+    return (
+     <div>
+        <p>hello</p>
+     </div>
+    );
+  }
+}
+
+export default GitRepo;
